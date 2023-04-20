3961a33a6e44faafcc76f2320390e401f16f69f7
Nicholas Padmanabhan <nicholaspad@gmail.com>
2023-04-19 22:05:31 -0400

    Add query param ref=princetoncourses

--- public/scripts/display.js
+++ public/scripts/display.js
@@ -170,7 +170,7 @@ var display_subtitle = function(course) {
       '<a href="https://tigersnatch.com/course' +
       "?courseid=" +
       course.courseID +
-      '&skip&princetoncourses" target="_blank" style="text-decoration: none;">' +
+      '&skip&ref=princetoncourses" target="_blank" style="text-decoration: none;">' +
       '<span title="View course in TigerSnatch" class="label label-warning">TigerSnatch' +
       '<i class="fa fa-external-link" style="margin-left: 5px;"></i>' +
       "</span></a>";
